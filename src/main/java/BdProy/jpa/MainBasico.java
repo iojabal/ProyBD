f17bdea8d95d5aae9135ca922815cd6206e22ce6
iojabal <iojabal9@gmail.com>
2023-05-08 21:54:21 -0400

    asd

--- src/main/java/BdProy/jpa/MainBasico.java
+++ src/main/java/BdProy/jpa/MainBasico.java
@@ -20,7 +20,7 @@ public class MainBasico {
 
         try {
             Alumno cliente = new Alumno("Juanqui", "Perez");
-            repositorio.insertar(client);
+            repositorio.insertar(cliente);
 //            repositorio.actualizarNombre(cliente.getId(), "Juanqui");
 //			repositorio.eliminar(cliente.getId());
 
